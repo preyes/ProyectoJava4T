211c27ecc23e3eeaaa7506ff1b19055430e9e8c0
preyes <pabloreyes460@gmail.com>
2021-08-25 17:57:21 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        String a = "Hola mundo";
+        System.out.println(a);
+
+        Auto auto2 = new Auto("FIAT", "AAA-123", "VERDE");
+
+        boolean z = true;
+        auto1.encender(z);
+
+    }
+}
---
+++ src/Auto.java
@@ -0,0 +1,22 @@
+public class Auto {
+
+    String marca;
+    String patente;
+    String color;
+    boolean encender;
+
+    Auto(String marca, String patente, String color) {
+
+        this.marca = marca;
+        this.patente = p;
+        this.color = c;
+
+    }
+
+    void encendido(boolean e) {
+
+        this.encender = e;
+
+    }
+
+}
