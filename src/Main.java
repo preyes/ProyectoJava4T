184c158a91a6c864fb965ac5f87a7b84d21f5fc6
preyes <pabloreyes460@gmail.com>
2021-09-16 17:28:59 -0300

    commit 4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class Main {
+    static Scanner teclado;
 
     public static void main(String[] args) {
 
@@ -13,10 +14,12 @@ public class Main {
 
         // Profesor prof1 = new Profesor(); prof1.materias = "Intro a la Programacion";
 
-        int numeros[] = new int[4];// declarando un arreglo de 4 elementos
+        // Declaracion de un arreglo de 4 elementos
+        int[] numeros = new int[4];
 
-        int arreglo[] = { 35, 89, 9, -92, 3, 63, -1, -43, 7, 6, 7, 34, 5 };
+        int[] arreglo = { 35, 89, 9, -92, 3, 63, -1, -43, 7, 6, 7, 34, 5 };
 
+        // Imprime la cantidad de elementos de un arreglo con .length
         System.out.println("cantidad de Elementos: " + arreglo.length);
 
         for (int f = 0; f < arreglo.length; f++) {
@@ -25,7 +28,7 @@ public class Main {
 
         }
 
-        String paises[] = { "Argentina", "Peru", "Brasil" };
+        String[] paises = { "Argentina", "Peru", "Brasil" };
 
         for (int i = 0; i < paises.length; i++) {
 
@@ -33,10 +36,13 @@ public class Main {
 
         }
 
-        // Scanner teclado = new Scanner(System.in);
-        System.out.println("Ingrese la cantidad de Provincias a procesar");
+        // Programa que ingresa la cantidad de datos a cargar en el arreglo
+        teclado = new Scanner(System.in);
+        int cantidad = 0;
+        System.out.println("Ingrese la cantidad de Provincias a cargar");
+        cantidad = teclado.nextInt();
 
-        String[] provincias = new String[4];// aqui va la cantidad de provincias
+        String[] provincias = new String[cantidad];// aqui va la cantidad de provincias
 
         for (int f = 0; f < provincias.length; f++) {
 
