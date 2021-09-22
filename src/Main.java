d2d83e70ff5e952a598113bdbff1ff4464ed48db
preyes <pabloreyes460@gmail.com>
2021-09-22 18:28:47 -0300

    commit5

--- src/Main.java
+++ src/Main.java
@@ -15,17 +15,27 @@ public class Main {
         // Profesor prof1 = new Profesor(); prof1.materias = "Intro a la Programacion";
 
         // Declaracion de un arreglo de 4 elementos
-        int[] numeros = new int[4];
+
+        int[] numeros = new int[4]; // [8,15,3,72]
+                                    // 0 1 2 3
+
+        for (int f = 4; f < 4; f++) {
+
+            System.out.println("Ingrese el numero");
+
+            numeros[f] = teclado.nextInt();// 8, 15, 3, 72
+
+        }
 
         int[] arreglo = { 35, 89, 9, -92, 3, 63, -1, -43, 7, 6, 7, 34, 5 };
 
         // Imprime la cantidad de elementos de un arreglo con .length
         System.out.println("cantidad de Elementos: " + arreglo.length);
-
+        int acum = 0;
         for (int f = 0; f < arreglo.length; f++) {
 
             System.out.println(arreglo[f]);
-
+            acum = arreglo[f] + acum;
         }
 
         String[] paises = { "Argentina", "Peru", "Brasil" };
@@ -38,16 +48,16 @@ public class Main {
 
         // Programa que ingresa la cantidad de datos a cargar en el arreglo
         teclado = new Scanner(System.in);
-        int cantidad = 0;
+        int cantidad = 0; // declaramos una variable cantidad
         System.out.println("Ingrese la cantidad de Provincias a cargar");
-        cantidad = teclado.nextInt();
+        cantidad = teclado.nextInt(); // 5
 
-        String[] provincias = new String[cantidad];// aqui va la cantidad de provincias
+        String[] provincias = new String[cantidad];// 5 aqui va la cantidad de provincias
 
         for (int f = 0; f < provincias.length; f++) {
 
             System.out.println("Ingrese la provincia");
-            provincias[f] = teclado.nextLine();
+            provincias[f] = teclado.next();
 
         }
 
---
+++ src/Registro.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Registro {
+
+    int[] notas;
+    String[] nombres;
+    Scanner teclado;
+
+    // CONSTRUCTOR
+    Registro() {
+
+        int cantidad = 0;
+        teclado = new Scanner(System.in);
+
+        System.out.println("Ingrese la cantidad de notas a procesar");
+        cantidad = teclado.nextInt();
+        notas = new int[cantidad];
+        nombres = new String[cantidad];
+
+        for (int i = 0; i < nombres.length; i++) {
+
+            System.out.println("Ingrese el nombre de cada alumno");
+            nombres[i] = teclado.next();
+            System.out.println("Ingrese la nota del alumno");
+            notas[i] = teclado.nextInt();
+
+        }
+
+    }
+
+    public void mostrarpromedionotas() {
+        System.out.println("Las notas son:" + notas);
+
+    }
+
+    public void notasmayores() {
+        System.out.println("La notas es mayor");
+
+    }
+
+    public void notasmenores() {
+        System.out.println("La nota es menor");
+    }
+
+}
